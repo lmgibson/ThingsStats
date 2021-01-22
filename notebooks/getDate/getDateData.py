e5cf6f5c98a045b1e924861440563700afbd31c2
lmgibson <landon.g42@gmail.com>
2021-01-22 14:19:35 -0800

    Running into an encoding issue on another computer. Can't replicate on mine so this is a wild guess if it will work. Fix 1.

--- notebooks/getDate/getDateData.py
+++ notebooks/getDate/getDateData.py
@@ -6,8 +6,9 @@ import os
 path = os.environ['HOME'] + \
     '/Library/Group Containers/JLMPQHK86H.com.culturedcode.ThingsMac/Things Database.thingsdatabase/main.sqlite'
 conn = sqlite3.connect(path)
+dateCreated = "Created: %s \n\n" % date.today().strftime("%Y-%m-%d")
 
-# Get date of task by task name
+# Get data for past week or month and save out to a file.
 if str.lower(sys.argv[1]) == 'week':
     query = "SELECT strftime('%Y-%m-%d', datetime(creationDate, 'unixepoch', 'localtime')) as date, title, \
             CASE WHEN notes = '' THEN 'No Note' ELSE notes END AS test \
@@ -16,9 +17,11 @@ if str.lower(sys.argv[1]) == 'week':
             ORDER BY date DESC"
     results = conn.execute(query).fetchall()
 
-    with open((os.environ['HOME'] + '/Desktop/data.txt'), 'w+') as fp:
-        fp.write("Created: " + date.today().strftime("%Y-%m-%d") + '\n\n')
-        fp.write('\n'.join('%s | %s | %s' % x for x in results))
+    with open((os.environ['HOME'] + '/Desktop/data.txt'), 'w+', encoding='utf-8-sig') as fp:
+        fp.write(dateCreated)
+        for val in results:
+            fp.write(('\n %s %s %s' %
+                      (val[0], val[1].encode('utf-8'), val[2].encode('utf-8'))))
 
 elif str.lower(sys.argv[1]) == 'month':
     query = "SELECT strftime('%Y-%m-%d', datetime(creationDate, 'unixepoch', 'localtime')) as date, title, \
@@ -28,9 +31,11 @@ elif str.lower(sys.argv[1]) == 'month':
             ORDER BY date DESC"
     results = conn.execute(query).fetchall()
 
-    with open((os.environ['HOME'] + '/Desktop/data.txt'), 'w+') as fp:
-        fp.write("Created: " + date.today().strftime("%Y-%m-%d") + '\n\n')
-        fp.write('\n'.join('%s | %s | %s' % x for x in results))
+    with open((os.environ['HOME'] + '/Desktop/data.txt'), 'w+', encoding='utf-8-sig') as fp:
+        fp.write(dateCreated)
+        for val in results:
+            fp.write(('\n %s %s %s' %
+                      (val[0], val[1].encode('utf-8-sig'), val[2].encode('utf-8-sig'))))
 
 else:
     print("""
