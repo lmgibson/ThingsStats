69b0f1a93b197663dddd632b8be5a78610e23611
lmgibson <landon.g42@gmail.com>
2021-01-23 14:39:42 -0800

    renamed the descriptive report script tp report.py. Cleaned up the code in getDateData.py by separating the argument conversion and the query execution so I'm not repeating myself

--- code/getDateData/getDateData.py
+++ code/getDateData/getDateData.py
@@ -8,33 +8,11 @@ path = os.environ['HOME'] + \
 conn = sqlite3.connect(path)
 dateCreated = "Created: %s \n\n" % date.today().strftime("%Y-%m-%d")
 
-# Get data for past week or month and save out to a file.
+# Converting argument to numeric value
 if str.lower(sys.argv[1]) == 'week':
-    query = "SELECT strftime('%Y-%m-%d', datetime(creationDate, 'unixepoch', 'localtime')) as date, title \
-            FROM TMTask \
-            WHERE date BETWEEN datetime('now', '-6 days') AND datetime('now', 'localtime') \
-            ORDER BY date DESC"
-    results = conn.execute(query).fetchall()
-
-    with open((os.environ['HOME'] + '/Desktop/' + date.today().strftime("%Y-%m-%d") + '_ThingsData.txt'), 'w+') as fp:
-        fp.write(dateCreated)
-        for val in results:
-            fp.write(('\n %s %s' %
-                      (val[0].encode('utf-8-sig'), val[1].encode('utf-8'))))
-
+    timeFrame = 7
 elif str.lower(sys.argv[1]) == 'month':
-    query = "SELECT strftime('%Y-%m-%d', datetime(creationDate, 'unixepoch', 'localtime')) as date, title \
-            FROM TMTask \
-            WHERE date BETWEEN datetime('now', '-30 days') AND datetime('now', 'localtime') \
-            ORDER BY date DESC"
-    results = conn.execute(query).fetchall()
-
-    with open((os.environ['HOME'] + '/Desktop/' + date.today().strftime("%Y-%m%-d") + '_ThingsData.txt'), 'w+') as fp:
-        fp.write(dateCreated)
-        for val in results:
-            fp.write(('\n %s %s' %
-                      (val[0].encode('utf-8-sig'), val[1].encode('utf-8-sig'))))
-
+    timeFrame = 30
 else:
     print("""
     After typing 'python ./getDateData.py' please indicate whether you
@@ -43,3 +21,17 @@ else:
     For example: python ./getDateData.py 'month' will get data from the
     past month.
             """)
+    quit()
+
+# Get data for past week or month and save out to a file.
+query = "SELECT strftime('%%Y-%%m-%%d', datetime(creationDate, 'unixepoch', 'localtime')) as date, title \
+        FROM TMTask \
+        WHERE date BETWEEN datetime('now', '-%s days') AND datetime('now', 'localtime') \
+        ORDER BY date DESC" % timeFrame
+results = conn.execute(query).fetchall()
+
+with open((os.environ['HOME'] + '/Desktop/' + date.today().strftime("%Y-%m-%d") + '_ThingsData.txt'), 'w+') as fp:
+    fp.write(dateCreated)
+    for val in results:
+        fp.write(('\n %s %s' %
+                  (val[0].encode('utf-8-sig'), val[1].encode('utf-8'))))
