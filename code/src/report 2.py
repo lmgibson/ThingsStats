2395af6e2e0d23fd60315d51d1c8943ab4ffb085
lmgibson <landon.g42@gmail.com>
2021-03-14 16:31:40 -0700

    Cleaning up unused code and focusing project on the report.py

---
+++ code/src/report 2.py
@@ -0,0 +1,25 @@
+from src import ThingsData as td
+from src import utilities
+from simple_term_menu import TerminalMenu
+import sys
+
+
+if __name__ == '__main__':
+
+    # Get requested timeframe
+    timeFrame = utilities.askForTimeFrame()
+
+    # Get stats
+    stats = td.statsReport(timeFrame)
+    createdTasks = stats.getNewTasks()
+    completedTasks = stats.getRecentCompletedTasks()
+
+    # Report to standard output
+    print("""
+        In the past %s days you have created %s tasks 
+        of which you have completed %s.\n""" %
+          (timeFrame, len(createdTasks), len(completedTasks)))
+
+    # Ask if user would like to see all created tasks
+    utilities.askPrintTasks(createdTasks)
+
