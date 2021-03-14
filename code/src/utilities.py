8e1c1a38111fd5b91d7782373375c3a79603a4b8
lmgibson <landon.g42@gmail.com>
2021-03-14 15:38:19 -0700

    Added doc strings to utilities

--- code/src/utilities.py
+++ code/src/utilities.py
@@ -1,4 +1,15 @@
 def parseInputs(inputsList):
+    """Parses command line inputs. Currently that is just
+    a string that indicates the time period to do the analysis.
+
+    Args:
+        inputsList (list): List of inputs given at the command
+        line.
+
+    Returns:
+        string: string that is then used to decide what time
+        frame to analyze the results over.
+    """
     if inputsList[1] in ['month', 'week']:
         timeFrame = inputsList[1]
     else:
@@ -8,7 +19,18 @@ def parseInputs(inputsList):
     return timeFrame
 
 
+def
+
+
 def askPrintTasks(createdTasks):
+    """Asks user if they would like to see the tasks they made
+    in the past week. Will repeat until the user answers yes
+    or no.
+
+    Args:
+        createdTasks (list): list of tuples containing information
+        on the tasks that have been created in the past week or month.
+    """
     printTasks = input(
         "Would you like to see the created tasks [y/N]?\n").lower()
 
