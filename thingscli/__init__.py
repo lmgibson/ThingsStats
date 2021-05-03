5945bb54383a8eed6bea58f264b693e0749f48d2
Landon <landon.g42@gmail.com>
2021-05-02 20:27:22 -0700

    Broken. Need to figure out how to make a command line executable

--- thingscli/__init__.py
+++
@@ -1,7 +0,0 @@
-from thingscli import main
-from thingscli import ThingsAddDaily
-from thingscli import utilities
-from thingscli import ThingsReport
-
-if __name__ == "__main__":
-    main.things()
